16b1c27df76b4dfc97f5c6cf1fd3dd644a1101a6
Adriano Lima <adrianolslima@gmail.com>
2017-02-12 16:14:39 -0200

    Add name and role to the header of the page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// $("#main").append("Adriano");
+
+// var awsomeThoughts = "I am Adriano and I am awsome!";
+
+// console.log(awsomeThoughts);
+
+// var funThoughts = awsomeThoughts.replace("awsome", "fun");
+
+// $("#main").append(funThoughts);
+
+var name = "Adriano Lima";
+var role = "Web Developer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
