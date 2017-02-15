10fc4d62e59fb2d9a9c0da4b6d833ba663dca4d4
Adriano Lima <adrianolslima@gmail.com>
2017-02-15 16:23:22 -0200

    Add some work and education info

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -31,31 +31,66 @@ var bio = {
 	"skills": skills
 };
 
+// HEADER
 var name = HTMLheaderName.replace("%data%", bio.name);
 var role = HTMLheaderRole.replace("%data%", bio.role);
-
-var mobile = HTMLmobile.replace("%data%", bio.contact.mobile);
-var email = HTMLemail.replace("%data%", bio.contact.email);
-var twitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
-var github = HTMLgithub.replace("%data%", bio.contact.github);
-var blog = HTMLblog.replace("%data%", bio.contact.blog);
-var city = HTMLlocation.replace("%data%", bio.contact.city);
-
 var pic = HTMLbioPic.replace("%data%", bio.pic);
-var msg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
-var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 
 $("#header").append(pic);
 $("#header").append(name);
 $("#header").append(role);
 
+// CONTACTS
+var mobile = HTMLmobile.replace("%data%", bio.contact.mobile);
+var email = HTMLemail.replace("%data%", bio.contact.email);
+var twitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
+var github = HTMLgithub.replace("%data%", bio.contact.github);
+var blog = HTMLblog.replace("%data%", bio.contact.blog);
+var bioCity = HTMLlocation.replace("%data%", bio.contact.city);
+
 $("#topContacts").append(mobile);
 $("#topContacts").append(email);
-$("#topContacts").append(twitter);
+// $("#topContacts").append(twitter);
 $("#topContacts").append(github);
-$("#topContacts").append(blog);
-$("#topContacts").append(city);
+// $("#topContacts").append(blog);
+$("#topContacts").append(bioCity);
+
+var msg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
 
 $("#header").append(msg);
 $("#header").append(HTMLskillsStart);
 $("#header").append(formattedSkills);
+
+var work = {
+};
+
+work.jobPosition = "boss";
+work.employer = "UFSC";
+work.yearsWorked = 8;
+work.city = "Florianópolis";
+
+var jobPosition = HTMLworkTitle.replace("%data%", work.jobPosition);
+var employer = HTMLworkTitle.replace("%data%", work.employer);
+// var
+var workCity = HTMLworkLocation.replace("%data%", work.city);
+
+$("#main").append(jobPosition);
+$("#main").append(employer);
+$("#main").append(work.yearsWorked);
+$("#main").append(workCity);
+
+var education = {
+};
+
+education["name"] = "Rosário";
+education["yearsStudied"] = 8;
+education["city"] = "Volta Redonda";
+
+var schoolName = HTMLschoolName.replace("%data%", education.name);
+var yearsStudied = HTMLschoolName.replace("%data%", education.yearsStudied);
+var educationCity = HTMLschoolName.replace("%data%", education.city);
+
+$("#main").append(schoolName);
+$("#main").append(yearsStudied);
+$("#main").append(educationCity);
