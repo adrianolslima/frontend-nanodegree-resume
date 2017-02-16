6cd7c5e9939fa7b179e4010b65fb3a2b4e9fffc8
Adriano Lima <adrianolslima@gmail.com>
2017-02-15 22:13:09 -0200

    Set up the template

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,20 +1,3 @@
-// $("#main").append("Adriano");
-
-// var awsomeThoughts = "I am Adriano and I am awsome!";
-
-// console.log(awsomeThoughts);
-
-// var funThoughts = awsomeThoughts.replace("awsome", "fun");
-
-// $("#main").append(funThoughts);
-
-// var name = "Adriano Lima";
-// var role = "Web Developer";
-
-
-
-var skills = ["programming", "teaching", "Java", "C/C++", "JS"];
-
 var bio = {
 	"name": "Adriano Lima",
 	"role": "Web Developer",
@@ -28,69 +11,98 @@ var bio = {
 	},
 	"pic": "https://placebeard.it/200/200",
 	"welcomeMsg": "Welcome!",
-	"skills": skills
+	"skills": ["programming", "teaching", "Java", "C/C++", "JS"]
 };
-
-// HEADER
-var name = HTMLheaderName.replace("%data%", bio.name);
-var role = HTMLheaderRole.replace("%data%", bio.role);
-var pic = HTMLbioPic.replace("%data%", bio.pic);
-
-$("#header").append(pic);
-$("#header").append(name);
-$("#header").append(role);
-
-// CONTACTS
-var mobile = HTMLmobile.replace("%data%", bio.contact.mobile);
-var email = HTMLemail.replace("%data%", bio.contact.email);
-var twitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
-var github = HTMLgithub.replace("%data%", bio.contact.github);
-var blog = HTMLblog.replace("%data%", bio.contact.blog);
-var bioCity = HTMLlocation.replace("%data%", bio.contact.city);
-
-$("#topContacts").append(mobile);
-$("#topContacts").append(email);
-// $("#topContacts").append(twitter);
-$("#topContacts").append(github);
-// $("#topContacts").append(blog);
-$("#topContacts").append(bioCity);
-
-var msg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
-var formattedSkills = HTMLskills.replace("%data%", bio.skills);
-
-$("#header").append(msg);
-$("#header").append(HTMLskillsStart);
-$("#header").append(formattedSkills);
-
-var work = {
-};
-
-work.jobPosition = "boss";
-work.employer = "UFSC";
-work.yearsWorked = 8;
-work.city = "Florianópolis";
-
-var jobPosition = HTMLworkTitle.replace("%data%", work.jobPosition);
-var employer = HTMLworkTitle.replace("%data%", work.employer);
-// var
-var workCity = HTMLworkLocation.replace("%data%", work.city);
-
-$("#main").append(jobPosition);
-$("#main").append(employer);
-$("#main").append(work.yearsWorked);
-$("#main").append(workCity);
 
 var education = {
+	"school": [
+		{
+			"name": "Universidade Federal de Santa Catarina",
+			"degree": "",
+			"dates": 2015,
+			"location": "Florianópolis, SC, Brazil",
+			"major": "BA"
+		}
+	],
+	"onLineCourses": [
+		{
+			"title": "JavaScript Basics",
+			"school": "Udacity",
+			"dates": 2017,
+			"url": "https://classroom.udacity.com/courses/ud804"
+		}
+	]
 };
 
-education["name"] = "Rosário";
-education["yearsStudied"] = 8;
-education["city"] = "Volta Redonda";
+var work = {
+	"jobs": [
+		{
+			"employer": "",
+			"title": "",
+			"dates": 2008,
+			"location": "Florianópolis",
+			"description": ""
+		}
+	]
+};
 
-var schoolName = HTMLschoolName.replace("%data%", education.name);
-var yearsStudied = HTMLschoolName.replace("%data%", education.yearsStudied);
-var educationCity = HTMLschoolName.replace("%data%", education.city);
+var projects = {
+	"projects": [
+		{
+			"title": "",
+			"date": 2013,
+			"description": "",
+			"images": ["", ""]
+		}
+	]
+}
 
-$("#main").append(schoolName);
-$("#main").append(yearsStudied);
-$("#main").append(educationCity);
+// // HEADER
+// var name = HTMLheaderName.replace("%data%", bio.name);
+// var role = HTMLheaderRole.replace("%data%", bio.role);
+// var pic = HTMLbioPic.replace("%data%", bio.pic);
+
+// $("#header").append(pic);
+// $("#header").append(name);
+// $("#header").append(role);
+
+// // CONTACTS
+// var mobile = HTMLmobile.replace("%data%", bio.contact.mobile);
+// var email = HTMLemail.replace("%data%", bio.contact.email);
+// var twitter = HTMLtwitter.replace("%data%", bio.contact.twitter);
+// var github = HTMLgithub.replace("%data%", bio.contact.github);
+// var blog = HTMLblog.replace("%data%", bio.contact.blog);
+// var bioCity = HTMLlocation.replace("%data%", bio.contact.city);
+
+// $("#topContacts").append(mobile);
+// $("#topContacts").append(email);
+// // $("#topContacts").append(twitter);
+// $("#topContacts").append(github);
+// // $("#topContacts").append(blog);
+// $("#topContacts").append(bioCity);
+
+// var msg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMsg);
+// var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+
+// $("#header").append(msg);
+// $("#header").append(HTMLskillsStart);
+// $("#header").append(formattedSkills);
+
+
+// var jobPosition = HTMLworkTitle.replace("%data%", work.jobPosition);
+// var employer = HTMLworkTitle.replace("%data%", work.employer);
+// var workCity = HTMLworkLocation.replace("%data%", work.city);
+
+// $("#main").append(jobPosition);
+// $("#main").append(employer);
+// $("#main").append(work.yearsWorked);
+// $("#main").append(workCity);
+
+
+// var schoolName = HTMLschoolName.replace("%data%", education.name);
+// var yearsStudied = HTMLschoolName.replace("%data%", education.yearsStudied);
+// var educationCity = HTMLschoolName.replace("%data%", education.city);
+
+// $("#main").append(schoolName);
+// $("#main").append(yearsStudied);
+// $("#main").append(educationCity);
